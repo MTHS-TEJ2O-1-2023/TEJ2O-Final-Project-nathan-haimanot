ab883209d4eae245a610ef4715a685233d61b5e5
nathan-haimanot <nathan.haimanot@mths.ca>
2025-01-13 11:15:43 -0500

    made phone scroller

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Nathan Tesfaye
+ * Created on: Jan 2025
  * This program ...
 */
 
-basic.showString('Hello, World!')
+
+// variables
+const servoNumber1 = robotbit.Servos.S1
+radio.setGroup(1)
+
+// recieve
+radio.onReceivedString(function (receivedString) {
+        robotbit.Servo(servoNumber1, 180)
+        basic.showIcon(IconNames.Happy)
+    
+    input.onButtonPressed(Button.B, function () {
+        robotbit.Servo(servoNumber1, 360)
+        basic.showIcon(IconNames.Happy)
+    })
+})
